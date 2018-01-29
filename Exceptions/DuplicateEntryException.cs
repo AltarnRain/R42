f5bc529ac67c7c21aec633b6368b82472e2fb41a
Onno <mabusraeen@gmail.com>
2018-01-29 23:00:50 +0100

    Moved Extentions library to its own folder

--- Exceptions/DuplicateEntryException.cs
+++ Exceptions/DuplicateEntryException.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="DuplicateAssetException.cs" company="OI">
+﻿// <copyright file="DuplicateEntryException.cs" company="OI">
 // Copyright (c) OI. All rights reserved.
 // </copyright>
 
@@ -26,7 +26,6 @@ namespace Exceptions
         /// Initializes a new instance of the <see cref="DuplicateEntryException" /> class.
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
-        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
         public DuplicateEntryException(string message)
             : base(message)
         {
