bf2d39e480a14adbe76d10bd9e1a473515aaa44c
Onno <mabusraeen@gmail.com>
2018-02-03 15:04:53 +0100

    Implemented Ninject. Removed AnimationModel and GameAssets model. Added providers to create models added a factroy for AssetManager.

---
+++ Providers/AssetTypeProvider.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="AssetTypeProvider.cs" company="OI">
+// Copyright (c) OI. All rights reserved.
+// </copyright>
+
+namespace Providers
+{
+    using System.Collections.Generic;
+    using Round42.Models;
+
+    /// <summary>
+    /// Providers AssetTypes
+    /// </summary>
+    public static class AssetTypeProvider
+    {
+        public static List<string> Types()
+        {
+
+        }
+    }
+}
