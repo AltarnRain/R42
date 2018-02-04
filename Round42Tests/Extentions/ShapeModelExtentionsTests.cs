6ee2de214aaab8a8403c38982777615cce66399a
Onno <mabusraeen@gmail.com>
2018-02-04 14:51:24 +0100

    Consolodated test projects into single test project.

---
+++ Round42Tests/Extentions/ShapeModelExtentionsTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Round242.Tests.Extentions
+{
+    public class ShapeModelExtentionsTests
+    {
+        
+    }
+}
