cb999b43df5f9892eb2ba344331c57686a2cb72c
Onno <mabusraeen@gmail.com>
2018-02-26 15:41:34 +0100

    Going to use MVVM for level editor

--- RoundEditor/Properties/Resources.Designer.cs
+++ RoundEditor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LevelEditor.Properties
+namespace RoundEditor.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace LevelEditor.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LevelEditor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RoundEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
